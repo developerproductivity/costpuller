a68b28632fc4d1e571a375ab0bfbce8f6f288d14
Webb Scales <wscales@redhat.com>
2026-02-04 13:47:10 -0500

    Switch Cloudability view from "unrestricted" to "726"

--- cloudability.go
+++ cloudability.go
@@ -146,7 +146,7 @@ func getCloudabilityData(configMap Configuration, options CommandLineOptions) *C
 			filtersAny, filtersAny)
 	}
 	//qParams.Add("filters", "unblended_cost>0")
-	qParams.Set("view_id", "0")
+	qParams.Set("view_id", "1751167")
 	qParams.Set("limit", "0")
 	path, err := url.JoinPath(cUrl.Path, uri)
 	if err != nil {
